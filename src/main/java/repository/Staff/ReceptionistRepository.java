de0a6dcd5df7ebd62566895a5dda481283ba9e2c
Zana <zana.ademi@student.uni-pr.edu>
2024-05-17 23:36:21 +0200

    AddReceptionist

---
+++ src/main/java/repository/Staff/ReceptionistRepository.java
@@ -0,0 +1,14 @@
+package repository.Staff;
+
+import model.dto.StaffDto.CreateReceptionistDto;
+
+public class ReceptionistRepository extends StaffRepository {
+    private static final String query = """
+            INSERT INTO receptionists(receptionist_firstName, receptionist_lastName, receptionist_birthdate, receptionist_phone, receptionist_email, receptionist_hashPassword, receptionist_salt, receptionist_address, receptionist_department, receptionist_specialization, receptionist_start, receptionist_end, bankName, bankAccount, routingNumber)
+            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+            """;
+
+    public static boolean createReceptionist(CreateReceptionistDto receptionistData) {
+        return createStaff(receptionistData, query);
+    }
+}
