28e910a8b7b4ea543c3e76a258f05212b87aea80
Fatjeta <fatjeta.gashi3@student.uni-pr.edu>
2024-05-21 01:55:58 +0200

    Update ReceptionistPageController and ReceptionistPage.fxml

--- src/main/java/controller/ReceptionistPageController.java
+++ src/main/java/controller/ReceptionistPageController.java
@@ -93,132 +93,137 @@ public class ReceptionistPageController
     @FXML
     private Button update_btn;
 
-
-    @FXML
-    private PasswordField recPassword;
-
-    @FXML
-    private TextField recPhone;
-
-    @FXML
-    private TextField recRoutingNr;
-
-    @FXML
-    private TextField recUni;
-
-    @FXML
-    private DatePicker recStart;
-
-
-
-
-
-
-
-
-    //@FXML
-    //private ComboBox<?> docDep;
-
-
-    @FXML
-    private TextField docAccount;
-
-    @FXML
-    private TextField docAddress;
-
-    @FXML
-    private TextField docBank;
-
-    @FXML
-    private DatePicker docBirthdate;
-
-    @FXML
-    private TextField docEmail;
-
-    @FXML
-    private DatePicker docEnd;
-
-    @FXML
-    private TextField docFirstName;
-
-    @FXML
-    private TextField docLastName;
-
-    @FXML
-    private PasswordField docPassword;
-
-    @FXML
-    private TextField docPhone;
-
-    @FXML
-    private TextField docRoutingNr;
-
-    @FXML
-    private TextField docSpecialization;
-
-    @FXML
-    private DatePicker docStart;
-
-    @FXML
-    private AnchorPane patient_table;
-
-
-
-    @FXML
-    private Button nurse_btn;
-
-    @FXML
-    private AnchorPane nurse_form;
-
-    @FXML
-    private TextField recSpecialization;
-
-
-    @FXML
-    private Button receptionist_btn;
-
-    @FXML
-    private AnchorPane receptionist_form;
-
-    @FXML
-    private AnchorPane register_doctor_form;
-
     @FXML
     private AnchorPane register_patient_form;
 
     @FXML
-    private AnchorPane register_nurse_form;
+    private TextField patFirstName;
 
     @FXML
-    private AnchorPane register_receptionist_form;
+    private TextField patLastName;
 
     @FXML
-    private TextArea txtDepartmentDescription;
+    private DatePicker patBirthdate;
 
     @FXML
-    private TextField txtDepartmentName;
+    private TextField patPhone;
+
+    @FXML
+    private TextField patEmail;
+
+    @FXML
+    private PasswordField patPassword;
+
+    @FXML
+    private TextField patAddress;
+
+    @FXML
+    private ComboBox<?> patDep;
+
+    @FXML
+    private ComboBox<?> patDoctor;
+
+    @FXML
+    private ComboBox<?> patNurse;
+
+    @FXML
+    private DatePicker patDate;
+
+    @FXML
+    private TextField patPayment;
+
+    @FXML
+    private Button register_patient_btn;
+
+
+    @FXML
+    private AnchorPane appointments_form;
+
+    @FXML
+    private TextField appID;
+
+    @FXML
+    private TextField appName;
+
+    @FXML
+    private RadioButton appMale ;
+
+    @FXML
+    private RadioButton appFemale ;
+
+    @FXML
+    private TextField appDesc;
+
+    @FXML
+    private ComboBox<?> appDep;
+
+    @FXML
+    private ComboBox<?> appDoctor;
+
+    @FXML
+    private ComboBox<?> appNurse;
+
+    @FXML
+    private TextField appPhone;
+
+    @FXML
+    private TextField appAddress;
+
+    @FXML
+    private DatePicker appDate;
+
+    @FXML
+    private TextField appHour;
+
+    @FXML
+    private TableView<?> app_tableView;
+
+    @FXML
+    private TableColumn<?, ?> app_col_appID;
+
+    @FXML
+    private TableColumn<?, ?> app_col_name;
+
+    @FXML
+    private TableColumn<?, ?> app_col_department;
+
+    @FXML
+    private TableColumn<?, ?> app_col_doctor;
+
+    @FXML
+    private TableColumn<?, ?> app_col_nurse;
+
+    @FXML
+    private TableColumn<?, ?> app_col_phone;
+
+    @FXML
+    private TableColumn<?, ?> app_col_address;
+
+    @FXML
+    private TableColumn<?, ?> app_col_date;
+
+    @FXML
+    private TableColumn<?, ?> app_col_hour;
+
+    @FXML
+    private Button addApp_btn;
+
+
+
     @FXML
     void switchForm(ActionEvent event) {
         if (event.getSource() == dashboard_btn) showForm(dashboard_form);
         else if (event.getSource() == patients_btn) showForm(patients_form);
-        else if (event.getSource() == appointments_btn) showForm(patients_form);
         else if (event.getSource() == account_btn) showForm(profile_form);
-      //  else if (event.getSource() == receptionist_btn) showForm(receptionist_form);
         else if (event.getSource() == add_patient_btn) showForm(register_patient_form);
-      //  else if (event.getSource() == add_nurse_btn) showForm(register_nurse_form);
-      //  else if (event.getSource() == add_receptionist_btn) showForm(register_receptionist_form);
-
+        else if (event.getSource() == appointments_btn) showForm(appointments_form);
     }
     private void showForm(AnchorPane form) {
         dashboard_form.setVisible(form == dashboard_form);
         patients_form.setVisible(form == patients_form);
-       // nurse_form.setVisible(form == nurse_form);
-        //accountant_form.setVisible(form == accountant_form);
         profile_form.setVisible(form == profile_form);
-        //receptionist_form.setVisible(form == receptionist_form);
-        //department_form.setVisible(form == department_form);
-        //register_doctor_form.setVisible(form == register_doctor_form);
-        //register_nurse_form.setVisible(form == register_nurse_form);
-        //register_receptionist_form.setVisible(form == register_receptionist_form);
+        register_patient_form.setVisible(form == register_patient_form);
+        appointments_form.setVisible(form == appointments_form);
+
     }
 }
